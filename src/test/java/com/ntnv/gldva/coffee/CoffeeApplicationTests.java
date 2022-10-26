050cf4eaca98f88ef9c8fe9183f77f5c6c83a0a6
Anton Milov <avm@gldva.ru>
2022-10-26 15:24:50 +0700

    Initial commit

---
+++ src/test/java/com/ntnv/gldva/coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntnv.gldva.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
